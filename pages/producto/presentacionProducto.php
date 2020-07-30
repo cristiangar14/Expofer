a11abcc49580b111b0c935a1eca5b37a7731d244
Cristian Garzon Sanchez <desarrollo.cristian.garzon@gmail.com>
2020-07-30 14:10:57 -0500

    con slider de productos

--- pages/producto/presentacionProducto.php
+++ pages/producto/presentacionProducto.php
@@ -12,22 +12,25 @@
             </li>
             <li id="slide2">
             <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/sunset.jpg"/>
-            </li>
-            <li id="slide3">
-            <h1>Ejemplo con otros elementos</h1>
-            <p>Esto es un párrafo de ejemplo para comprobar que podemos meter cualquier tipo de elementos en el slider</p>
-            <a href="https://kikopalomares.com/">¡Corre a mi web para más contenido!</a>
+            </li id="slide3">
+                <div class="imagenes-producto-grande">
+                    <div id="descuentoCard" class="producto-con-descuento">
+                        <p class="valor-descuento">10%</p>
+                        <p class="off-descuento">OFF</p>
+                    </div>
+                    <img src="../expofer/img/paginaProducto/aspiradora.png" alt="aspiradora">
+                </div>
             </li>
         </ul>
-        <ul class="menu">
+        <ul class="menu-slider">
             <li>
-            <a href="#slide1">1</a>
+            <a href="#slide1"><img  class="imagenes-producto-lateral" src="../expofer/img/paginaProducto/aspiradora.png" alt="aspiradora"></a>
             </li>
             <li>
-            <a href="#slide2">2</a>
+            <a href="#slide2"><img  class="imagenes-producto-lateral" src="../expofer/img/paginaProducto/aspiradora.png" alt="aspiradora"></a>
             </li>
             <li>
-            <a href="#slide3">3</a>
+            <a href="#slide3"><img  class="imagenes-producto-lateral" src="../expofer/img/paginaProducto/aspiradora.png" alt="aspiradora"></a>
             </li>
             <li>
             <a href="#slide4">4</a>
